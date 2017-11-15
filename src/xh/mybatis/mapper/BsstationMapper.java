7b1b806a1ea2dce4a0129c7e72ca8a068af2deb0
wangliuking <1287818687@qq.com>
2017-11-15 10:10:04 +0800

    2017/11/15 第七次提交

--- src/xh/mybatis/mapper/BsstationMapper.java
+++ src/xh/mybatis/mapper/BsstationMapper.java
@@ -147,7 +147,7 @@ public interface BsstationMapper {
 	public List<HashMap<String,String>> rectangle(Map<String,Object> map)throws Exception;
 	
 	/**
-	 * 圈选基站总数
+	 * 圈选基站总数 dd
 	 * @return
 	 * @throws Exception
 	 */
