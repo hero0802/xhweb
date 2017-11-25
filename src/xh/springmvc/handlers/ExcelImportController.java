429c5cdadc41409abdc7f84548749ca46854cd90
wangliuking <1287818687@qq.com>
2017-11-25 18:59:20 +0800

    2017/11/25 不规则圈选

--- src/xh/springmvc/handlers/ExcelImportController.java
+++ src/xh/springmvc/handlers/ExcelImportController.java
@@ -24,18 +24,34 @@ public class ExcelImportController {
 	      
 	    @RequestMapping(value="/upload",method = RequestMethod.POST)  
 	    @ResponseBody  
-	    public String upload(@RequestParam(value="file",required = false)MultipartFile file,HttpServletRequest request, HttpServletResponse response){
+	    public void upload(@RequestParam(value="file",required = false)MultipartFile file,HttpServletRequest request, HttpServletResponse response){
 	    	String bsId = request.getParameter("bsId");
 	    	String roadTime = request.getParameter("roadTime");
 	        String result = excelService.readExcelFile(file,roadTime,bsId);
-	        /*response.setContentType("application/json;charset=utf-8");
+	        response.setContentType("application/json;charset=utf-8");
 			String jsonstr = json.Encode(result);
 			try {
 				response.getWriter().write(jsonstr);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}*/
-	        return result;
+			}
+	    }
+	    
+	    /**
+	     * 上传其他excel文件测试
+	     */
+	    @RequestMapping(value="/uploadOther",method = RequestMethod.POST)  
+	    @ResponseBody  
+	    public void uploadOther(@RequestParam(value="file",required = false)MultipartFile file,HttpServletRequest request, HttpServletResponse response){
+	        String result = excelService.readExcelFileOther(file);
+	        response.setContentType("application/json;charset=utf-8");
+			String jsonstr = json.Encode(result);
+			try {
+				response.getWriter().write(jsonstr);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	    }
 }
